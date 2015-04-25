579614defa547a1650512ea60098dbbabf79a5e2
Claire <Claire>
2015-04-25 15:06:29 -0700

    Removed event data - new architecture involves new events simply inheriting from the base event class.

--- source/events/event.d
+++ source/events/event.d
@@ -1,19 +1,16 @@
 import event_receiver;
 
-class EventData
-{
-    long eventID;    
-}
-
 class Event
 {
-    EventData data;
-    void SendEvent(EventReceiver target)
+    private long eventID;
+
+    public void SendEvent(EventReceiver target)
     {
         if (target.CanHandle(this))
         {
             //Send the event
         }
+
     }
     
 }
