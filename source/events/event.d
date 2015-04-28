f6bdfbe1e1f35494cec59a49cb13bae90608eedf
Claire <Claire>
2015-04-28 15:06:04 -0700

    Created Event Constructor.

--- source/events/event.d
+++ source/events/event.d
@@ -3,10 +3,17 @@ import event_receiver;
 class Event
 {
     private long eventID;
-
+    
     private static long currID = 0;
     private static long[string] stringMap;
     private static string[long] longMap;
+
+    public Event(string name)
+    {
+        
+        eventID = AssignID(name);
+
+    }
 
     public void SendEvent(EventReceiver target)
     {
@@ -16,10 +23,25 @@ class Event
         }
     }
 
-    static public long GenerateID()
+    static private long GenerateID()
     {
         ++currID;
         return currID;
+    }
+
+    static private long AssignID(string name)
+    {
+        long newID;
+        long *id;
+        id = name in stringMap;
+        if (id == null)
+        {
+            newID = GenerateID();
+            stringMap[name] = newID;
+            longMap[newID] = name;
+            id = &newID;
+        }
+        return *id;
     }
     
 }
