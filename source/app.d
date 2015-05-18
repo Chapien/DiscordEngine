646c539a3d1f7344a7d804e3e0770e47115a17af
chapien <claire@chapien.net>
2015-05-18 12:58:52 -0700

    Tested events. It works!

--- source/app.d
+++ source/app.d
@@ -1,6 +1,21 @@
 import std.stdio;
+import event;
+import event_receiver;
 
 void main()
 {
-    writeln("Edit source/app.d to start your project.");
+    EventReceiver space = new EventReceiver();
+    Event e = new Event("update");
+
+    void delegate(Event) del;
+    space.RegisterEvent(e.GetID, &(new C().handle));
+    e.SendEvent(space);
+}
+
+class C
+{
+    public void handle(Event e)
+    {
+        writeln("Event Received!");
+    }
 }
